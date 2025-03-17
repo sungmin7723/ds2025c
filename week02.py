2895e410237f5b3bbf3c8b2d55a2235f2c349e88
hyeon <smyoun115@gmail.com>
2025-03-17 15:40:03 +0900

    v0.3 O(log n)

--- week02.py
+++ week02.py
@@ -1,5 +1,26 @@
-n = int(input("정수를 입력하세요 : "))
+import random
 
-result = (n * (n + 1)) // 2  # 상수 시간 O(1)
+randomNum =  random.randint(1, 100)
 
-print(result)
+win = False
+
+for i in range(7, 0, -1) :
+    print(f"{i}번 남았음")
+
+    guessNum = int(input("숫자 입력 : "))
+
+    if guessNum == randomNum :
+        print("정답입니다")
+        win = True
+        break
+
+    elif guessNum > randomNum :
+        print("더 낮음.")
+
+    elif guessNum < randomNum :
+        print("더 높음.")
+
+if win :
+    print("you win")
+else :
+    print(f"정답 {randomNum}인데 이걸 못 맞추네 ㅋ 어휴 ㅉㅉ ㅄ 수준 ㅋ")
