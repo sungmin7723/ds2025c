ef944823697970feea3f1e742d30b4b5b640dadd
hyeon <smyoun115@gmail.com>
2025-03-24 15:15:27 +0900

    v0.9  inters

--- week03.py
+++ week03.py
@@ -1,19 +1,12 @@
-def duplicate(cities):
-    result_city = list()
-    s = set()
+def inters(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
 
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result_city.append(city)
-    return result_city
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 41]
 
-cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon','Incheon', 'Seoul']
+print(inters(l1, l2))
 
-cities.append('Seoul')
-cities.append('Anyang')
-
-print(cities)
-print(set(duplicate(cities)))
