9e2de5cd6ba2a1f42d3581f1fc2453221e9fbdba
hyeon <smyoun115@gmail.com>
2025-03-24 15:18:51 +0900

    v1.0 set interscetion

--- week03.py
+++ week03.py
@@ -1,9 +1,8 @@
 def inters(l1, l2):
-    l3 = list()
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+    s1 = set(l1)
+    s2 = set(l2)
+    # return list(s1.intersection(s2))
+    return list(s1 & s2)
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 27, 19, 41]
