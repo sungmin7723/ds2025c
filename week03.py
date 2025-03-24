62da033904369977f42436cef4982d95f1e36491
hyeon <smyoun115@gmail.com>
2025-03-24 14:56:58 +0900

    v0.7 set

--- week03.py
+++ week03.py
@@ -1,7 +1,4 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-ratings = [1, 2, 4, 3]
+cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Seoul']
+cities  = set(cities)
 
-
-groups_rating = list(zip(groups, ratings))
-# zip 함수는 대응되는 인덱스 끼리 묶어 튜플로 반환 (두 인덱스의 크기가 다를땐 작은 크기 만큼 생성)
-print(groups_rating)
+print(cities)
