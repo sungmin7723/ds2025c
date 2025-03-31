4f1c2b81cf1d47b644fd8c0ee72f71c40902fe7f
hyeon <smyoun115@gmail.com>
2025-03-31 14:34:02 +0900

    v1.2 Linked List (falied)

--- week03.py
+++ week03.py
@@ -8,4 +8,13 @@ l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 27, 19, 41]
 
 print(inters(l1, l2))
+'''
+def my_zip(l1, l2):
+    if l1
+    r = []
+    for i in range(len(l1)):
+        r.append((l1[i], l2[i]))
+             
+    return r
 
+'''
--- week04.py
+++ week04.py
@@ -9,13 +9,28 @@ class LinkedList:
         self.head = None
 
     def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
+        # if not self.head:
+        #     self.head = Node(data)
+        #     return
+        # current = self.head
+        # while current.link:
+        #     current = current.link # 이동
+        # current.link = Node(data)
         current = self.head
-        while current.link:
-            current = current.link # 이동
-        current.link = Node(data)
+        out_texts = ""
+        while current is not None:
+            out_texts = out_texts + str(current.data) + " -> "
+            current= current.link
+        return out_texts + " END"
+
+    def __str__(self):
+        current = self.head
+        out_texts = ""
+        while current is not None:
+            out_texts = out_texts + str(current.data) + " -> "
+            current = current.link
+        return out_texts + " END"
+
 
 ll = LinkedList()
 
