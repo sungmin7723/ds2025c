6b5252a540a2cbb7c45638717c2085fef332ef9c
sungmin <smyoun115@gmail.com>
2025-04-28 14:53:32 +0900

    2.1 TreeNode in, pre, post

--- week08.py
+++ week08.py
@@ -1,8 +1,33 @@
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end='-')
+
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end='-')
+    in_order(node.right)
+
+
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end='-')
+    pre_order(node.left)
+    pre_order(node.right)
+
+
 class TreeNode:
     def __init__(self):
         self.left = None
         self.data = None
         self.right = None
+
 
 node1 = TreeNode()
 node1.data = 'hs'
@@ -27,5 +52,8 @@ node6 = TreeNode()
 node6.data = 'sm'
 node3.left = node6
 
-print(node5.data)
-print(node1.left.right.data)
+post_order(node1)
+print()
+in_order(node1)
+print()
+pre_order(node1)
